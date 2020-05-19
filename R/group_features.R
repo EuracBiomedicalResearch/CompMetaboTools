80bb75ecbee726be76109e2f7d7aba5c99177583
jorainer <johannes.rainer@gmail.com>
2020-05-19 13:02:25 +0200

    fix: ensure correlation-based grouping works

--- R/group_features.R
+++ R/group_features.R
@@ -227,12 +227,18 @@
                 mcor_2_2 <- mean(cor_2_2, na.rm =TRUE)
                 ## Put the elements into the group of the other, if its
                 ## correlation to its group is larger than to its own group
-                if (!(is.na(mcor_1_2) || any(cor_1_2 < threshold))
-                    && mcor_1_2 > mcor_1_1)
+                if (is.finite(mcor_1_2) && is.finite(mcor_1_1) &&
+                    !any(cor_1_2 < threshold) && mcor_1_2 > mcor_1_1)
                     res[idx_pairs[i, 1]] <- got_grp[2]
-                if (!(is.na(mcor_2_1) || any(cor_2_1 < threshold))
-                    && mcor_2_1 > mcor_2_2)
+                if (is.finite(mcor_2_1) && is.finite(mcor_2_2) &&
+                    !any(cor_2_1 < threshold) && mcor_2_1 > mcor_2_2)
                     res[idx_pairs[i, 2]] <- got_grp[1]
+                ## if (!(is.na(mcor_1_2) || any(cor_1_2 < threshold))
+                ##     && is.finite(mcor_1_1) && mcor_1_2 > mcor_1_1)
+                ##     res[idx_pairs[i, 1]] <- got_grp[2]
+                ## if (!(is.na(mcor_2_1) || any(cor_2_1 < threshold))
+                ##     && is.finite(mcor_2_2) && mcor_2_1 > mcor_2_2)
+                ##     res[idx_pairs[i, 2]] <- got_grp[1]
             } # else nothing to do - they are already in the same group
         }
     }
